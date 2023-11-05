bd5061b4b3482800be1a66b2299c4b776733ee5c
+Sam Thongo <+samuelthongo73@gmail.com>
2023-11-05 20:24:47 +0300

    freebies

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref, sessionmaker
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -16,6 +17,21 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    def freebies(self):
+        return self.freebies
+
+    def devs(self):
+        return [freebie.dev for freebie in self.freebies]
+
+    def give_freebie(self, session, dev, item, value):
+        freebie = Freebie(dev=dev, item=item, value=value)
+        session.add(freebie)
+        session.commit()
+
+    @classmethod
+    def oldest_company(self):
+        return session.query(Company).order_by(Company.founding_year).first()
+
     def __repr__(self):
         return f'<Company {self.name}>'
 
@@ -25,5 +41,51 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+    def freebies(self):
+        return self.freebies
+
+    def companies(self):
+        return [freebie.company for freebie in self.freebies]
+
+    def received_freebie(self,item):
+        for freebie in self.freebies:
+            if freebie.item == item:
+                return True
+            return False
+
+    def give_freebie(self, session, other_dev, freebie):
+        if freebie.dev == self:
+            freebie.dev = other_dev
+            session.commit()
+
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item = Column(String())
+    value = Column(Integer())
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+
+    @property
+    def _dev(self):
+        return self._dev
+
+    @property
+    def _company(self):
+        return self._company
+
+    def print_details(self):
+        return f"{self._dev.name} owns a {self.item} from {self._company.name} with a value of {self.value}"
+
+    def __repr__(self):
+        return f'<Freebie {self.item}>'
+
+engine = create_engine("sqlite:///freebies.db")
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,48 @@
-#!/usr/bin/env python3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Company, Dev, Freebie
 
-# Script goes here!
+engine = create_engine("sqlite:///freebies.db")
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Clear the existing data
+session.query(Company).delete()
+session.query(Dev).delete()
+session.query(Freebie).delete()
+session.commit()
+
+# Sample data
+company1 = Company(name="Company A", founding_year=1990)
+company2 = Company(name="Company B", founding_year=2000)
+company3 = Company(name="Company C", founding_year=2010)
+
+dev1 = Dev(name="Dev 1")
+dev2 = Dev(name="Dev 2")
+dev3 = Dev(name="Dev 3")
+
+# Create Freebie instances and associate them with Dev and Company
+freebie1 = Freebie(item="T-shirt", value=10)
+freebie1.dev = dev1
+freebie1.company = company1
+
+freebie2 = Freebie(item="Sticker", value=2)
+freebie2.dev = dev2
+freebie2.company = company2
+
+freebie3 = Freebie(item="Mug", value=5)
+freebie3.dev = dev1
+freebie3.company = company3
+
+freebie4 = Freebie(item="T-shirt", value=15)
+freebie4.dev = dev3
+freebie4.company = company2
+
+# Add and commit the sample data
+session.add_all([company1, company2, company3, dev1, dev2, dev3, freebie1, freebie2, freebie3, freebie4])
+session.commit()
+
+# Print the Freebie instances
+for freebie in session.query(Freebie).all():
+    print(freebie)
